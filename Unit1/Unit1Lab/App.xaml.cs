b6a8378cb375b27781713ac7bc1784c59a4826f6
erikbasto <30513989+erikbasto@users.noreply.github.com>
2023-03-16 18:00:01 -0600

    Lab1

---
+++ Unit1/Unit1Lab/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Unit1Lab;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Unit1/Unit1Lab/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Unit1Lab;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
